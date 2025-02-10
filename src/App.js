54193e9df5e437fbf12d3ed072f88996a39e8f62
RanaMohamed30 <ranaelsherif91@gmail.com>
2025-02-10 17:34:46 +0200

    add login 02

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import "./styles/shared.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/login/login";
-import Register from "./components/register/register";
+import Login from "./components/signInUp/login/login";
+import Register from "./components/signInUp/register/register";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />}>
-            {/* <Route index element={<Home />} /> */}
-            <Route path="/register" element={<Register />} />
-          </Route>
+          <Route path="/login" element={<Login />} />
+          {/* <Route index element={<Home />} /> */}
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
       {/* <Login /> */}
--- src/components/signInUp/login/login.js
+++ src/components/signInUp/login/login.js
@@ -3,31 +3,18 @@ import { Formik } from "formik";
 import { Row, Col, Container, Button } from "react-bootstrap";
 import { useTranslation } from "react-multi-lang";
 import LoginForm from "./loginForm";
-import LoginBg from "./loginBg";
 import { Link } from "react-router-dom";
-import "./login.scss";
+import SignInUpShared from "../signInUpShared";
+import "../SignInUp.scss";
 function Login() {
   const t = useTranslation();
   return (
-    <div className="loginSection">
+    <div className="SignSection">
       {/* <Container> */}
       <Row className="justify-content-md-center">
-        <Col md={4} xs={6}>
+        <Col md={5} xs={12}>
           <div className="login_form">
-            <h2> {t("Login.HeaderTitle")}</h2>
-            <p>{t("Login.HeaderSubTitle")}</p>
-            <Button
-              // type="submit"
-              //disabled={this.state.progressVariant == "danger" || this.state.userErr}
-              className="frmBtn transBtn FullWidthBtn"
-            >
-              {t("Login.LoginWithGoogle")}
-            </Button>
-            <p className="or_line_tit">
-              <span className="or_line"></span>
-              <span>{t("Login.Or")}</span>
-              <span className="or_line"></span>
-            </p>
+            <SignInUpShared login={true} />
             <LoginForm />
             <p className="form_option">
               {t("Login.DontHaveAccount")}
@@ -35,8 +22,10 @@ function Login() {
             </p>
           </div>
         </Col>
-        <Col md={8} xs={6}>
-          <LoginBg />
+        <Col md={7} className="d-none d-sm-block">
+          <div className="login_img_bg sign-img_bg">
+            {/* <img src="images/login_bg_img.png" alt="wasla_login_img" /> */}
+          </div>
         </Col>
       </Row>
       {/* </Container> */}
