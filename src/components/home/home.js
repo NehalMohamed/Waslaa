98a2fe1db076233c77b40d04af262e006b5ce974
NehalMohamed <nehal.mohamed040@gmail.com>
2025-02-15 00:46:53 +0200

    home new update

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,144 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
-  Navbar,
-  Nav,
-  Container,
   Button,
-  Dropdown,
   Row,
   Col,
+  Card
 } from "react-bootstrap";
-import {
-  FaCommentDots,
-  FaBullhorn,
-  FaFacebookF,
-  FaInstagram,
-  FaTwitter,
-  FaEnvelope,
-  FaWhatsapp,
-} from "react-icons/fa";
-import { GoSearch } from "react-icons/go";
-import { FiUser } from "react-icons/fi";
-import { CiGlobe, CiMenuFries } from "react-icons/ci";
+import MainNavbar from '../navbars/mainNavbar';
+import SaleNavbar from '../navbars/saleNavbar';
+import MainFooter from '../footer/mainFooter';
+import Chat from '../chatIcon/chat';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./home.scss";
 
 const Home = () => {
-  const [targetDate] = useState(() => {
-    const date = new Date();
-    date.setDate(date.getDate() + 60);
-    return date;
-  });
-
-  const calculateTimeLeft = () => {
-    const now = new Date();
-    const difference = targetDate - now;
-
-    if (difference <= 0) {
-      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
-    }
-
-    return {
-      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-      minutes: Math.floor((difference / 1000 / 60) % 60),
-      seconds: Math.floor((difference / 1000) % 60),
-    };
-  };
-
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, [targetDate]);
-
   return (
-    <Container fluid className="p-0">
-      {/* Navbar */}
-      <Navbar fixed="top" expand="lg" className="navbar-custom py-3">
-        <Navbar.Brand href="#" className="brand d-flex align-items-center">
-          <img src="logo/wasla logo.png" alt="Logo" className="logo" />
-        </Navbar.Brand>
-        <Nav className="nav-items d-none d-lg-flex">
-          <Nav.Link href="#" className="nav-item">
-            HOME
-          </Nav.Link>
-          <Nav.Link href="#" className="nav-item">
-            PRICING
-          </Nav.Link>
-          <Nav.Link href="#" className="nav-item">
-            OUR WORK
-          </Nav.Link>
-        </Nav>
-        <div className="ms-auto d-flex gap-3 align-items-center nav-icons">
-          <GoSearch className="icon" />
-          <FiUser className="icon" />
-          <CiGlobe className="icon" />
-          <Dropdown>
-            <Dropdown.Toggle
-              variant="light"
-              id="dropdown-basic"
-              className="dropdown-toggle-custom"
-            >
-              <CiMenuFries className="icon" />
-            </Dropdown.Toggle>
-            <Dropdown.Menu align="end" className="dropdown-menu-custom">
-              <Row className="">
-                <Col md={6} className="">
-                  <Dropdown.Item href="#" className="dropdown-item-custom">
-                    <FaCommentDots className="me-2" /> CHAT
-                  </Dropdown.Item>
-                </Col>
-                <Col md={6} className="">
-                  <Dropdown.Item href="#" className="dropdown-item-custom">
-                    <FaBullhorn className="me-2" /> MARKETING
-                  </Dropdown.Item>
-                </Col>
-              </Row>
-              <Row className="">
-                <Col md={6} className="">
-                  <Dropdown.Item href="#" className="dropdown-item-custom">
-                    <FaCommentDots className="me-2" /> CHAT
-                  </Dropdown.Item>
-                </Col>
-                <Col md={6} className="">
-                  <Dropdown.Item href="#" className="dropdown-item-custom">
-                    <FaBullhorn className="me-2" /> MARKETING
-                  </Dropdown.Item>
-                </Col>
-              </Row>
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-      </Navbar>
-      {/* Fixed Sale Navbar with Countdown */}
-      <Navbar fixed="top" className="sale-navbar py-2">
-        <Container
-          fluid
-          className="d-flex justify-content-between align-items-center"
-        >
-          <span className="sale-text">
-            Our Huge <span className="highlight">Sale Ends in</span>
-          </span>
-          <div className="countdown-timer">
-            {`${timeLeft.days} : ${timeLeft.hours} : ${timeLeft.minutes} : ${timeLeft.seconds}`}
-            <div className="countdown-labels">
-              <span className="countdown-label">Days</span>
-              <span className="countdown-label">Hrs</span>
-              <span className="countdown-label">Mins</span>
-              <span className="countdown-label">Secs</span>
-            </div>
-          </div>
-          <Button className="check-button">Check it out</Button>
-        </Container>
-      </Navbar>
-      <Container className="content-section">
+    <>
+      <MainNavbar />
+      
+      <SaleNavbar />
+      <div className="content-section">
         {/* Hero Section */}
-        <Container className="hero-section">
+        <div className="hero-section">
           <Row className="align-items-center">
             <Col md={6} className="text-center text-md-start">
               <h1 className="hero-heading">
@@ -159,12 +41,12 @@ const Home = () => {
               />
             </Col>
           </Row>
-        </Container>
+        </div>
 
         {/* Services Section */}
-        <Container className="services-section">
+        <div className="services-section">
           <Row className="service-item align-items-center mb-4">
-            <Col md={7}>
+            <Col md={6}>
               <h3 className="service-title">Design your brand identity</h3>
               <p className="service-description">
                 Do you have the project idea but <br />
@@ -173,7 +55,7 @@ const Home = () => {
                 <br /> stage?
               </p>
             </Col>
-            <Col md={5} className="text-center">
+            <Col md={6} className="text-center">
               <img
                 src="images/confused.png"
                 alt="Brand Identity"
@@ -181,9 +63,9 @@ const Home = () => {
               />
             </Col>
           </Row>
-        </Container>
+        </div>
 
-        <Container>
+        <div className="contact-designers">
           <Row className="service-item align-items-left">
             <Col md={6} className="design-text">
               <h3 className="design-title">Design your website</h3>
@@ -217,8 +99,9 @@ const Home = () => {
               />
             </Col>
           </Row>
-        </Container>
-        <Container>
+        </div>
+
+        <div className="market-section">
           <Row className="service-item align-items-center">
             <Col md={7}>
               <h3 className="service-title">
@@ -238,9 +121,109 @@ const Home = () => {
               />
             </Col>
           </Row>
-        </Container>
+        </div>
 
-        <Container>
+        <div className="how-work-section text-center my-15">
+            {/* HOW IT WORKS Section */}
+            <h5 className="how-work-text">HOW IT WORKS</h5>
+            <h2 className="three-steps-text">Get your services in just three steps.</h2>
+
+            <Row className="my-4">
+              <Col md={4}>
+                <Card className="p-4 border-0">
+                  <img src="/images/submit-request.png" alt="Submit Request" className="steps-img-small mb-3" />
+                  <h5>Submit your request</h5>
+                  <p>Fill out the form for the service you <br/>need and complete the payment <br/>process.</p>
+                </Card>
+              </Col>
+              <Col md={4}>
+                <Card className="p-4 border-0">
+                  <img src="/images/designers-work.png" alt="Designers Work" className="steps-img mb-3" />
+                  <h5>Our designers get to work</h5>
+                  <p>Designers will start working immediately <br/>and communicate with you to achieve <br/>the best results.</p>
+                </Card>
+              </Col>
+              <Col md={4}>
+                <Card className="p-4 border-0">
+                  <img src="/images/receive-design.png" alt="Receive Design" className="steps-img mb-3" />
+                  <h5>Receive your design</h5>
+                  <p>Receive your complete service. Give us <br/>
+                  your feedback.
+                  <br />
+                  <br />
+                  </p>
+                </Card>
+              </Col>
+            </Row>
+
+            <Button className="transBtn SmallWidthBtn roundedBtn fw-bold pickup-btn">Pick up a plan</Button>
+            {/* WHAT WE OFFER Section */}
+        </div>
+
+        <div className="how-work-section text-center my-15">
+            <h5 className="how-work-text text-center">WHAT WE OFFER</h5>
+            <h2 className="three-steps-text text-center">A design solution you will love</h2>
+
+            <Row className="my-4">
+              <Col md={6}>
+                <Card className="p-4 text-left border-0">
+                  <Row>
+                    <Col md={3}>
+                     <img src="/images/fast-reliable.png" alt="Fast & Reliable" className="offer-img" />
+                    </Col>
+                    <Col className="offer-Text" md={9}>
+                    <h5>Fast & Reliable</h5>
+                    <p>Get your service as soon as possible through <br/>continuous communication.</p>
+                    </Col>
+                  </Row>
+                </Card>
+              </Col>
+              <Col md={6}>
+                <Card className="p-4 text-left border-0">
+                <Row>
+                <Col md={3}>
+                  <img src="/images/integrated-brand.png" alt="Integrated Brand" className="offer-img" />
+                </Col>
+                <Col className="offer-Text" md={9}>
+                  <h5>Integrated brand</h5>
+                  <p>Create an integrated brand that makes your <br/>services easily accessible.</p>
+                </Col>
+                </Row>
+                </Card>
+              </Col>
+            </Row>
+
+            <Row className="mb-4">
+              <Col md={6}>
+                <Card className="p-4 text-left border-0">
+                <Row>
+                <Col md={3}>
+                  <img src="/images/collaborative-team.png" alt="Collaborative Team" className="offer-img" />
+                  </Col>
+                  <Col className="offer-Text" md={9}>
+                  <h5>Collaborative team</h5>
+                  <p>A design and marketing team collaborates to <br/>lead your brand to success.</p>
+                  </Col>
+                  </Row>
+                </Card>
+              </Col>
+              <Col md={6}>
+                <Card className="p-4  border-0">
+                <Row>
+                <Col md={3}>
+                  <img src="/images/budget-flexible.png" alt="Budget Flexible" className="offer-img" />
+                  </Col>
+                  <Col className="offer-Text" md={9}>
+                  <h5>Budget flexible</h5>
+                  <p>Plan according to your budget and the <br/>services you need.</p>
+                  </Col>
+                  </Row>
+                </Card>
+              </Col>
+            </Row>
+        </div>
+
+        <div>
           <div
             className="hero-container"
             style={{ backgroundImage: "url(/images/wasla-bg.png)" }}
@@ -256,80 +239,16 @@ const Home = () => {
             </div>
             <button className="hero-button">Get started</button>
           </div>
-        </Container>
-        {/* Footer */}
-        <footer className="footer py-4">
-          <Container className="p-0">
-            <hr className="custom-hr" />
-            <Row className="footer-content">
-              <Col md={4} className="footer-logo">
-                <img
-                  src="/logo/wasla logo.png"
-                  alt="Logo"
-                  className="footer-logo-img"
-                />
-                <p className="small-text footer-description">
-                  Waslaa aims to make your brand successful <br />
-                  through the access of your customers to <br />
-                  your brand with ease.
-                </p>
-                <div className="social-icons">
-                  <div className="icon">
-                    <FaFacebookF />
-                  </div>
-                  <div className="icon">
-                    <FaInstagram />
-                  </div>
-                  <div className="icon">
-                    <FaTwitter />
-                  </div>
-                  <div className="icon">
-                    <FaEnvelope />
-                  </div>
-                </div>
-              </Col>
-              <Col md={4} className="footer-links">
-                <h5>Company</h5>
-                <ul>
-                  <li className="small-text">Home</li>
-                  <li className="small-text">About</li>
-                  <li className="small-text">Pricing</li>
-                  <li className="small-text">Contact</li>
-                  <li className="small-text">Privacy</li>
-                </ul>
-              </Col>
-              <Col md={4} className="footer-support">
-                <h5>Support</h5>
-                <ul>
-                  <li className="small-text">Help Center</li>
-                  <li className="small-text">Chat with us</li>
-                  <li>
-                    {" "}
-                    <br />
-                  </li>
-                </ul>
-                <h5>Our New</h5>
-                <p className="small-text link-us">#Link_it_with_waslaa</p>
-              </Col>
-            </Row>
-            <hr className="custom-hr" />
-            <p className="text-left small-text copy-righ">
-              Copyright © Waslaa Inc. 2024-2025 - All Rights Reserved
-            </p>
-          </Container>
-        </footer>
+        </div>
 
-        {/* WhatsApp Floating Button */}
-        <a
-          href="https://wa.me/01067551474"
-          className="whatsapp-button"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaWhatsapp className="whatsapp-icon" />
-        </a>
-      </Container>
-    </Container>
+      </div>
+
+      <MainFooter />
+
+      {/* WhatsApp Floating Button */}
+       <Chat />
+    
+    </>
   );
 };
 
